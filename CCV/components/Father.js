1ff1a551f9171421bdc52b42c8634f194b8673ed
Cesar Minaya <57270301+Cesarminaya@users.noreply.github.com>
2021-01-02 15:08:46 -0400

    Add files via upload
    Update to version 1.0

--- CCV/components/Father.js
+++ CCV/components/Father.js
@@ -2,14 +2,17 @@ Vue.component('Father', {
     template:
         `
     <div class="p-5 bg-dark text-white text-center">
-    <h2 class="text-center pb-5">Números de padres: {{ FatherNumber }}</h2>
-    <button type="button" class="btn btn-warning mb-5" @click="FatherNumber++">NUMBER INCREMENT</button>
-    <Son :number="FatherNumber"></Son>
+    <h2 class="text-center pt-1">Números de padres: {{ FatherNumber }}</h2>
+    <h2 class="mb-5">{{ FatherName + ' ' + FatherLastName }}</h2>
+    <button type="button" class="btn btn-warning mb-5" @click="FatherNumber++"><b>+</b></button>
+    <Son :number="FatherNumber" @SonName="FatherName=$event" @SonLastName="FatherLastName=$event"></Son>
     </div>
     `,
-    data(){
-        return{
-            FatherNumber: 0
+    data() {
+        return {
+            FatherNumber: 0,
+            FatherName: '',
+            FatherLastName: ''
         }
     }
 }) 
--- CCV/components/Son.js
+++ CCV/components/Son.js
@@ -1,9 +1,22 @@
-Vue.component('Son',{
-    template: 
-    `
+Vue.component('Son', {
+    template:
+        `
     <div class="py-5 bg-success text-white">
     <h2 class="text-center">Componente Hijo: {{ number }} </h2>
+    <h2>{{ name + ' ' + lastname }}</h2>
     </div>
     `,
-    props:['number']
+    props: ['number'],
+    data() {
+        return {
+            name: 'Cesar',
+            lastname: 'Minaya'
+        }
+    },
+    // Mounted en el ciclo de vida establece que se ejecutara cuando carguen los datos.
+    mounted() {
+        this.$emit('SonName', this.name);
+        this.$emit('SonLastName', this.lastname);
+
+    }
 }) 
