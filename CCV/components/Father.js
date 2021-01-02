75a12df539422ac7c1d6678df59be0e9d2cb7534
Cesar Minaya <57270301+Cesarminaya@users.noreply.github.com>
2021-01-02 15:32:29 -0400

    Add files via upload
    update to version 1.1

--- CCV/components/Father.js
+++ CCV/components/Father.js
@@ -2,7 +2,7 @@ Vue.component('Father', {
     template:
         `
     <div class="p-5 bg-dark text-white text-center">
-    <h2 class="text-center pt-1">Números de padres: {{ FatherNumber }}</h2>
+    <h2 class="text-center pt-1">Componente Padre: {{ FatherNumber }}</h2>
     <h2 class="mb-5">{{ FatherName + ' ' + FatherLastName }}</h2>
     <button type="button" class="btn btn-warning mb-5" @click="FatherNumber++"><b>+</b></button>
     <Son :number="FatherNumber" @SonName="FatherName=$event" @SonLastName="FatherLastName=$event"></Son>
